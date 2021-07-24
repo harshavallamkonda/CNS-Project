c73713378485675455de4ae0d8e298b1b6968758
Vasudev <kingzvasu660@gmail.com>
2021-07-24 15:39:19 +0530

    Initiated Backend

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-var CryptoJS = require("crypto-js");
- 
-// Encrypt
-var ciphertext = CryptoJS.AES.encrypt('my message', 'secret key 123').toString();
- 
-// Decrypt
-console.log(ciphertext);
-var bytes  = CryptoJS.AES.decrypt(ciphertext, 'secret key 123');
-var originalText = bytes.toString(CryptoJS.enc.Utf8);
- 
-console.log(originalText);
+const express = require('express');
+const bodyparser = require('body-parser');
+const crypto = require('crypto');
+const cryptoJs = require('crypto-js');
+const nodeRsa = require('node-rsa');
+const nodeTripleDes = require('node_triple_des');
+const path = require('path');
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+app.listen(3000, function(req,res){
+        console.log("Server Fired Up and Listening at Post 3000");
+      }
+    );
+
+app.get('/', function(req,res){
+    res.
+    status(200).
+    sendFile(path.join(__dirname, "public","index.html"));
+});
+
+app.get('/encrypt', function(req,res){
+    res.
+    status(200).
+    sendFile(path.join(__dirname, "public","encrypt.html"));
+});
+app.get('/about', function(req,res){
+    res.
+    status(200).
+    sendFile(path.join(__dirname, "public","about.html"));
+});
+app.get('/packages', function(req,res){
+    res.
+    status(200).
+    sendFile(path.join(__dirname, "public","packages.html"));
+});
