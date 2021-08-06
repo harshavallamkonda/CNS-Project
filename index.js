3816cd868c1e15e451b736d48aec90a63df52908
Vasudev <kingzvasu660@gmail.com>
2021-08-06 20:29:45 +0530

    Updating Logo and initiating backend

--- index.js
+++ index.js
@@ -6,31 +6,4 @@ const nodeRsa = require('node-rsa');
 const nodeTripleDes = require('node_triple_des');
 const path = require('path');
 
-const app = express();
-app.use(express.static(path.join(__dirname, 'public')));
-app.listen(3000, function(req,res){
-        console.log("Server Fired Up and Listening at Post 3000");
-      }
-    );
 
-app.get('/', function(req,res){
-    res.
-    status(200).
-    sendFile(path.join(__dirname, "public","index.html"));
-});
-
-app.get('/encrypt', function(req,res){
-    res.
-    status(200).
-    sendFile(path.join(__dirname, "public","encrypt.html"));
-});
-app.get('/about', function(req,res){
-    res.
-    status(200).
-    sendFile(path.join(__dirname, "public","about.html"));
-});
-app.get('/packages', function(req,res){
-    res.
-    status(200).
-    sendFile(path.join(__dirname, "public","packages.html"));
-});
